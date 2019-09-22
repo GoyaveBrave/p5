8c701df11c0931ee00ba3e13014b20315b8d88b1
GoyaveBrave <majid.web98@gmail.com>
2019-09-22 09:59:39 +0200

    Create allPostspage.html.php

---
+++ view/allPostspage.html.php
@@ -0,0 +1,79 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="view/css/bootstrap.css">
+    <link rel="stylesheet" href="view/css/style.css">
+    <title>Postpage</title>
+    <!--NavBar-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="#">Navbar</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNav">
+<ul class="navbar-nav">
+  <li class="nav-item active">
+    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Blog</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Portfolio</a>
+  </li>
+  <li class="nav-item">
+    <button type="button" class="btn btn-primary">SignIn</button>
+  </li>
+</ul>
+</div>
+</nav>
+
+    
+      <!--Section1-->
+      <h2>All Posts</h2>
+      <?php foreach ($articles as $article): ?>
+      <div class="section3">
+        <div class="posts_prev">
+          <img src="img/blanc.jpg" alt="">
+        <p><?= $article['category'] ?></p>
+        <p><?= $article['title'] ?></p>
+        <p><?= $article['text'] ?>
+        </p>
+        <div class="art_bar"></div>
+        </div>
+        </div>
+      </div>
+      <?php endforeach; ?>
+
+      
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <button type="button" class="btn btn-primary">SignIn</button>
+        </li>
+      </ul>
+      </div>
+      </nav>
+  <? require '../index.php'; ?>
+</head>
+<body>
+    
+</body>
+</html>
