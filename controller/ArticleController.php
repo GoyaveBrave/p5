c568b7e4d7ae71eb465a7e9a179bc3a9a45e707f
GoyaveBrave <majid.web98@gmail.com>
2019-09-22 09:59:07 +0200

    Create ArticleController.php

---
+++ controller/ArticleController.php
@@ -0,0 +1,35 @@
+<?php
+require ('../model/Article.php');
+require ('../model/DatabaseConnexion.php');
+require ('../model/ArticleManager.php');
+//showAll
+
+class Controller {
+
+    //DB connexion
+    private $pdo;
+
+    public function __construct()
+     {
+     $this->pdo = getPdo();
+     }
+
+    //Call the manager
+    public function Controller()
+    {
+     $articleMana = new ArticleManager($pdo);
+    //Call the view
+    include('allPostspage.html.php');
+    }
+
+    public function articleController($id)
+    //DB connexion
+    {
+    $articleMana = new ArticleManager($pdo);
+    $article = $articleMana->find($id, 'Article');
+
+    include('View/postpage.html.php');
+    }
+
+}
+//show($id)
